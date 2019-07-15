a3bb955faffab44c08cf907bebf830639d1d1ff9
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-15 16:45:56 +0545

    Add example

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,9 @@
 package iteration
 
-func Repeat(character string, repeatCount int) string {
+// Repeat returns a new string consisting of count copies of the string s.
+func Repeat(character string, count int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < count; i++ {
 		repeated += character
 	}
 	return repeated

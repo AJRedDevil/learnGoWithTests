1ea8d3f54195a5c5fcf2bfdf25c1ae2a98315a82
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-07-29 15:17:52 +1000

    feat: Sum All the n number of slices into one slice

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,20 @@
 package main
 
+// Sum calculates the total from a slice of numbers.
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll calculates the sums of all the number giveon of collection of slices.
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }

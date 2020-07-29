f0bd7fbab0a33c65e3f9fa5e35744bc1c4018f65
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-07-29 14:22:35 +1000

    feat: Take repeat as input and add example doc

--- for/repeat.go
+++ for/repeat.go
@@ -1,8 +1,6 @@
 package iteration
 
-const repeatedCount = 5
-
-func Repeat(character string) string {
+func Repeat(character string, repeatedCount int) string {
 	var repeated string
 	for i := 0; i < repeatedCount; i++ {
 		repeated += character
--- for/repeat_test.go
+++ for/repeat_test.go
@@ -1,9 +1,12 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("b")
+	repeated := Repeat("b", 5)
 	expected := "bbbbb"
 
 	if repeated != expected {
@@ -11,8 +14,14 @@ func TestRepeat(t *testing.T) {
 	}
 }
 
+func ExampleRepeat() {
+	repeatedString := Repeat("y", 3)
+	fmt.Println(repeatedString)
+	// Output: yyy
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("z")
+		Repeat("z", 5)
 	}
 }

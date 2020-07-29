21a27607bfa1bfc47d3008ec0ad902e0047fc647
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-07-29 14:48:41 +1000

    docs: Add func doc

--- for/repeat.go
+++ for/repeat.go
@@ -1,9 +1,10 @@
 package iteration
 
-func Repeat(character string, count int) string {
+// Repeat returns a new string consisting of count copies of the string s
+func Repeat(s string, count int) string {
 	var repeated string
 	for i := 0; i < count; i++ {
-		repeated += character
+		repeated += s
 	}
 	return repeated
 }

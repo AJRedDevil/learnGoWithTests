7ee1e7422eede4946103b2c6d8d756f5be7c1295
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-07-31 09:55:55 +1000

    feat: Add add feature in dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,10 +2,12 @@ package main
 
 import "errors"
 
+// Dictionary store definitions to words.
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
 
+// Search find a word in the dictionary.
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
@@ -13,4 +15,9 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+// Add inserts a word and definition into the dictionary.
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
